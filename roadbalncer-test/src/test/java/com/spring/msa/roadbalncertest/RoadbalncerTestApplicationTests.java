b423ed32898fd5709a516ead7c45ef112b1ff604
sean <ngs1214@gmail.com>
2025-01-23 09:18:31 +0900

    feat: Gateway와 Eureka 연동하여 로드밸런싱처리

---
+++ roadbalncer-test/src/test/java/com/spring/msa/roadbalncertest/RoadbalncerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.msa.roadbalncertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadbalncerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
